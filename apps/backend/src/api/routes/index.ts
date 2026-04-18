6066d7c304cd8d3d908250a0c87da432ab5cff32
mattmalicki <mateusz.malicki96@gmail.com>
2026-04-18 16:02:05 +0200

    Accounts controller, routes and test are finished.

--- apps/backend/src/api/routes/index.ts
+++ apps/backend/src/api/routes/index.ts
@@ -1,11 +1,13 @@
 import { Router } from "express";
 import authRoutes from "./authRoutes";
+import accountRoutes from "./accountRoutes";
 import categoryRoutes from "./categoryRoutes";
 import transactionRoutes from "./transactionRoutes";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/account", accountRoutes);
 router.use("/category", categoryRoutes);
 router.use("/transaction", transactionRoutes);
 
--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -2,5 +2,6 @@ export * from "./types/api";
 export * from "./types/user";
 export * from "./types/icons";
 export * from "./schemas/auth";
+export * from "./schemas/accounts";
 export * from "./schemas/categories";
 export * from "./schemas/transactions";
