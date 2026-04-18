0960063e26064d7efe09dce2b8b4bdbe216dad5f
mattmalicki <mateusz.malicki96@gmail.com>
2026-04-18 16:29:07 +0200

    Routes done.

--- apps/backend/src/api/routes/index.ts
+++ apps/backend/src/api/routes/index.ts
@@ -3,6 +3,7 @@ import authRoutes from "./authRoutes";
 import accountRoutes from "./accountRoutes";
 import categoryRoutes from "./categoryRoutes";
 import transactionRoutes from "./transactionRoutes";
+import recurringPaymentRoutes from "./recurringPayment";
 
 const router = Router();
 
@@ -10,5 +11,6 @@ router.use("/auth", authRoutes);
 router.use("/account", accountRoutes);
 router.use("/category", categoryRoutes);
 router.use("/transaction", transactionRoutes);
+router.use("/recurringPayment", recurringPaymentRoutes);
 
 export default router;
