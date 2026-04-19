ab173a0e027526f3ce44e01bb5f2c4a3cbaaf54f
mattmalicki <mateusz.malicki96@gmail.com>
2026-04-19 13:16:44 +0200

    Changing title page of the auth page

--- apps/frontend/src/pages/AuthPage/index.tsx
+++ apps/frontend/src/pages/AuthPage/index.tsx
@@ -1,16 +1,16 @@
-import { Helmet } from 'react-helmet'
+import { Helmet } from "react-helmet";
 
 const AuthPage = () => {
   return (
     <>
       <Helmet>
-        <title>Auth</title>
+        <title>My Simple Wallet</title>
       </Helmet>
       <main>
         <h1>Auth Page</h1>
       </main>
     </>
-  )
-}
+  );
+};
 
-export default AuthPage
+export default AuthPage;
