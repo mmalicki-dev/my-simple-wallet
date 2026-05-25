aa9e8fe9a6924eb3a07cded7ab6ea2bde6f3992d
mattmalicki <mateusz.malicki96@gmail.com>
2026-05-25 12:07:17 +0200

    Loading screen needs to be worked on.

--- apps/mobile/src/screens/HomeScreen/Home.tsx
+++ apps/mobile/src/screens/HomeScreen/Home.tsx
@@ -4,12 +4,20 @@ import { useGetAccountsQuery } from "@/services";
 import { useSelector } from "react-redux";
 import { RootState } from "@/redux/store";
 import { HudPanel } from "@/components/templates/HudPanel/HudPanel";
+import { Text } from "react-native";
 
 const HomeScreen = () => {
   const { data: accounts = [], isLoading } = useGetAccountsQuery();
   const totalBalanceCurrency = useSelector(
     (state: RootState) => state.auth.user?.totalBalanceCurrency,
   );
+
+  if (isLoading)
+    return (
+      <ScreenLayout>
+        <Text>Loading</Text>
+      </ScreenLayout>
+    );
 
   return (
     <ScreenLayout>
